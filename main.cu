67ccb617bb1fd9139730a738c02959412d51c798
John Naylor <jonaylor89@gmail.com>
2020-10-01 18:02:40 -0400

    testing

--- main.cu
+++ main.cu
@@ -158,6 +158,7 @@ __global__ void KNN_GPU(float* dataset, int rows, int columns, int k, int* predi
         int* outputValues = new int[k];
         for(int j = 0; j < k; j++)
         {
+            printf("%d \n", neighbors[j] * columns + colummns - 1);
             outputValues[j] = (int)dataset[(neighbors[j] * columns) + columns - 1];
         }
 
